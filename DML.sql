fad9d7649fbcc848dc7794dc4d925446dc2dd280
Honde Kajal Santosh <hondekajal@gmail.com>
2026-01-31 21:23:06 -0800

    DML

--- DML.sql
+++ DML.sql
@@ -21,8 +21,25 @@ values
 (2, 'F','Pranjal','loe bp','2004-6-19'),
 (3, 'M','Priya','low bp','2005-8-19');
 
+-- changing column name
+alter table patients_records
+change column medical_history medical text;
 drop table patients_records;
 
 select * from patients_records
 
+-- insert records with less column names
+    insert into patients_records
+    (patient_id, gender, fullname)
+    values 
+    (4,'M','tanmay more');
+    
+    -- while inserting records it's important to insert in sequence
+    -- it should column name and values
+    
+     insert into  patients_records
+     (patient_id,fullname, gender)
+     values
+     (5,'pruthvi','m');
+ 
 describe patients_records;
